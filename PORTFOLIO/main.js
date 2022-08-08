ef203fb60abd067bb189e15f3b8c0547151fe13d
fredmash <108887930+fredmash@users.noreply.github.com>
2022-08-08 23:25:20 +0300

    Add files via upload

---
+++ PORTFOLIO/main.js
@@ -0,0 +1,10 @@
+var navlinks = document.getElementById("navlinks");
+function showmenu(){
+    navlinks.style.right = "0";
+        }
+function showmenu(){
+    navlinks.style.right = "-200";
+        }
+    
+       
+    
